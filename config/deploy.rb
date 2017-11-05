6d19e38c571408bd5afeb71396c2fb0e06af0024
jmarzin <jacques.marzin@free.fr>
2017-11-05 16:58:14 +0100

    samedi 4 novembre 2017

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'https://github.com/jmarzin/monsaintmoiruby.git'
 set :deploy_to, '/home/deploy/monsaintmoiruby'
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
