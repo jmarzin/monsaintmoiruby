8d758ac11c30e8a316bed833e1f65ef2673224b3
jmarzin <jacques.marzin@free.fr>
2017-11-11 17:32:00 +0100

    samedi 11 novembre 2017

--- mylibrary.rb
+++
@@ -1,7 +0,0 @@
-module MyLibrary
-  extend FFI::Library
-  ffi_lib Rails.root.joint('distance.so')
-  # double lat1, double lon1, double lat2, double lon2
-  # return distance in meters
-  attach_function :getDistance, [:double, :double, :double, :double], :double
-end
